2ba86517c276c93b33b95b88e9c2f37d96f7721b
Theresa <TreeGee73@gmail.com>
2021-01-07 22:37:09 -0700

    debug possibly

--- js/connections.js
+++ js/connections.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  // port: 3306,
   user: "root",
   password: "root",
   database: "trackingDB",
